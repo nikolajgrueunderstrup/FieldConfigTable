81b04dda246cb90864b87fada4fa790ab15126ac
Sebastian Kobylarz <sebastian.kobylarz@alstomgroup.com>
2018-10-22 18:13:20 +0200

    Updated JSON structure, corrected header css and cleaned PHP script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 var modal = document.getElementById('myModal');
 var span = document.getElementsByClassName("close")[0];
 
-function setEvents(){
-  $(".tablerows").click(function(){
+function setEvents() {
+  $(".tablerows").click(function () {
     $(this).css('background-color', '#ffdc61');
     modal.style.display = "block";
   });
@@ -14,14 +14,14 @@ function setEvents(){
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
+  span.onclick = function () {
+    modal.style.display = "none";
   }
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 }
